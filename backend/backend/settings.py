50484490117d3773ff615c4b08d90403f5b38f47
MartellOnell <mt102005@mail.ru>
2023-07-03 11:53:10 +0600

    fix connect to date base from backend
    (rewrite host to "dblocalhost")

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -71,22 +71,22 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'USER': 'root',
-#        'PASSWORD': 'nddr6444',
-#        'NAME': 'djdb',
-#        'PORT': '5432',
-#        'HOST': 'localhost',
-#    }
-#}
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "mydatabase",
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'USER': 'root',
+       'PASSWORD': 'nddr6444',
+       'NAME': 'djdb',
+       'PORT': '5432',
+       'HOST': 'dblocalhost',
+   }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": "mydatabase",
+#     }
+# }
 
 
 # Password validation
